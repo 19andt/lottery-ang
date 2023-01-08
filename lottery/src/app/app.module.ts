785cdf0701c05317d7683412cb32f6f7ab7fa629
Anand T <19andt@gmail.com>
2023-01-08 23:20:23 +0530

    live results component added

--- lottery/src/app/app.module.ts
+++ lottery/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LiveResultsComponent } from './live-results/live-results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LiveResultsComponent
   ],
   imports: [
     BrowserModule,
---
+++ lottery/src/app/live-results/live-results.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-live-results',
+  templateUrl: './live-results.component.html',
+  styleUrls: ['./live-results.component.css']
+})
+export class LiveResultsComponent {
+
+}
